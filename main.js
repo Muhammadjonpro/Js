364076a9cc010ae82ec2affb5f3a95c55147a15a
Muhammadjonpro <rahmatullayevm711@gmail.com>
2022-07-05 00:18:23 +0500

    JS

---
+++ main.js
@@ -0,0 +1,15 @@
+var nameUser = prompt("ismingiz");
+var surnameUser = prompt("Familiyangiz");
+var userageUser = prompt("Yoshimgiz");
+var hobbiesUser = prompt("Hobbiyingiz");
+var fromUser = prompt("Qayerliksiz");
+var workUser = prompt("ishlashjoyingiz");
+
+
+
+console.log(`Ismingiz:${nameUser}
+Familiyangiz:${surnameUser}
+Yoshingiz ${userageUser}
+Hobbiyingiz ${hobbiesUser} 
+Qayerliksiz ${fromUser}
+ishlashjoyingiz ${workUser}`); 
